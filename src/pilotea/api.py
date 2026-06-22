2ae4c8718d11cecd0e94865fb8deff8170e97e0c
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2026-06-22 19:27:34 +0100

    api imports

--- src/pilotea/api.py
+++ src/pilotea/api.py
@@ -1,4 +1,8 @@
+import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
